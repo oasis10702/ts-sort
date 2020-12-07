e454aaae3f74f5099fc4d4d4c9971e1fa0c7cc97
oasis10702 <oasis10702@gmail.com>
2020-12-07 11:45:04 +0900

    implement linkedlist

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import { Sorter } from './Sorter';
 import { NumberCollection } from './NumberCollection';
 import { CharacterCollection } from './CharacterCollection';
+import { LinkedList } from './LinkedList';
 
 const numberCollection = new NumberCollection([10000, 3, -5, 0]);
 const charactreCollection = new CharacterCollection('Xaayb');
-const sorter = new Sorter(charactreCollection);
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactreCollection.data);
+linkedList.print();
